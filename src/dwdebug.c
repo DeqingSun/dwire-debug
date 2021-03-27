0df9efa1bbd472ed647edd99303cb50a103b9f05
Deqing Sun <iamseer@gmail.com>
2021-03-26 21:37:11 -0400

    use dwen

--- src/dwdebug.c
+++ src/dwdebug.c
@@ -18,12 +18,12 @@ int main(int argCount, char **argVector) {
   if (argCount>4){
     if ( (strcmp(argVector[1], "-c")==0) && (strcmp(argVector[2], "gdb_port 50000")==0) && (strcmp(argVector[3], "-s")==0) ){
       fprintf( stderr, "Seems it is called by cortex-debug, do a dirty fix by replacing arg.\n");
-      char newArgVect1[] = "verbose,gdbserver,device";
-      char newArgVect2[] = "usbtiny1";
-      //verbose,gdbserver,device usbtiny1
+      const char newArgVect1[] = "dwen,verbose,gdbserver,device";
+      const char newArgVect2[] = "usbtiny1";
+      //dwen,verbose,gdbserver,device usbtiny1
       argCount = 3;
-      argVector[1]=newArgVect1;
-      argVector[2]=newArgVect2;
+      argVector[1]=(char *)newArgVect1;
+      argVector[2]=(char *)newArgVect2;
     }
   }
   
